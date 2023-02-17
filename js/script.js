cdc651a5f8d0f18e74fc5e972d302c7552c16b17
MartinaPerozzi <perozzimartina@gmail.com>
2023-02-17 16:43:07 +0100

    Emails

--- js/script.js
+++ js/script.js
@@ -11,12 +11,16 @@ const app = Vue.createApp({
 
     methods: {
         generatEmails() {
+            // Copia array (forse non serve)
             const emailsCopy = [];
+            // Per un ciclo di 10 volte ricihedi una email esterna randomica con API Boolean
             for (let i = 0; i < 10; i++) {
                 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
                     .then((response) => {
                         console.log(response.data.response);
+                        // Crea variabile con risultato che ci serve dell'oggettone email prodotto da API Boolean
                         let generatedEmail = response.data.response;
+                        // Pushali nell'array in data
                         this.emails.push(generatedEmail);
                     });
 
